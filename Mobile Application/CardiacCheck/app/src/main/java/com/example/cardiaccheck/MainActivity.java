282e1a554013e1d013155076adbb0944c0d4bd8f
Athindu <athinduama@gmail.com>
2020-05-04 22:30:38 +0530

    Mobile Application Final

---
+++ Mobile Application/CardiacCheck/app/src/main/java/com/example/cardiaccheck/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.cardiaccheck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("http://3.133.148.178/");
+        webView.setWebViewClient(new WebViewClient());
+    }
+
+
+    @Override
+    public void onBackPressed() {
+
+        if (webView.canGoBack()){
+            webView.goBack();
+        }else {
+            super.onBackPressed();
+        }
+    }
+}
